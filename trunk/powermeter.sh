f25ac0325bd9002783bcbe0b9951768267eef1e4
andywhite <andywhite@e09147e5-0f70-9c80-0d1c-ffc4a5762c21>
2011-01-19 15:40:29 +0000

    vi

--- trunk/powermeter.sh
+++ trunk/powermeter.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sh genpm.sh > /tmp/pm
-curl --silent --data-binary "@/tmp/pm"  --header "Content-Type: application/atom+xml" --header "Authorization: AuthSub token=putinurtoken" "https://www.google.com/powermeter/feeds/user/userid/userid/variable/devicename.d1/durMeasurement" >> /tmp/curl.log 
+sh /root/genpm.sh > /tmp/pm
+
+#curl --silent --data-binary "@/tmp/pm"  --header "Content-Type: application/atom+xml" --header "Authorization: AuthSub token=CNWZs4KhFRCn7sWs_v____8BGM78lIgG" "https://www.google.com/powermeter/feeds/user/07733944393685196730/07733944393685196730/variable/DM.ADM15TE.abcdefg.d1/durMeasurement" >> /tmp/curl.log 
   

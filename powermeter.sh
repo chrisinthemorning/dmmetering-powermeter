068210f0f21f31127cf21446e95602217d9c348e
andywhite <andywhite@e09147e5-0f70-9c80-0d1c-ffc4a5762c21>
2011-01-19 15:56:41 +0000

    fix

--- powermeter.sh
+++ powermeter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sh /root/genpm.sh > /tmp/pm
 
-#curl --silent --data-binary "@/tmp/pm"  --header "Content-Type: application/atom+xml" --header "Authorization: AuthSub token=yourtoken" "https://www.google.com/powermeter/feeds/user/userid/userid/variable/devicename.d1/durMeasurement" >> /tmp/curl.log 
+curl --silent --data-binary "@/tmp/pm"  --header "Content-Type: application/atom+xml" --header "Authorization: AuthSub token=yourtoken" "https://www.google.com/powermeter/feeds/user/userid/userid/variable/devicename.d1/durMeasurement" >> /tmp/curl.log 
   
